5ab8057d51971293e3cd3d61d4110a37f0a34fdd
vladimirlozickiy <volodya199996@gmail.com>
2019-02-17 13:11:30 +0300

    fix start programm

--- src/main/java/HostPart.java
+++ src/main/java/HostPart.java
@@ -14,9 +14,8 @@ import java.util.Locale;
 import java.util.stream.IntStream;
 import java.util.stream.*;
 import java.util.Random;
-import java.lang.*;
+import java.lang.Math;
 import java.io.*;
-
 
 
 import static org.jocl.CL.*;
@@ -28,26 +27,23 @@ public class HostPart {
     // размер коммуникатора
     private static final int maxGlobalWorkSize =2;
     private static final int xpoints = 1000;
-    private static final int save_freq =1; // частота сохранения результата
-    private static double tpoints = 40000.0;
+    private static double save_freq =1; // частота сохранения результата
     private static double dt = 0.001;
     private static double Rleft = 1.0;
-    private static double tmax=4700.0;
+    private static int tmax=900;
     private static double Rright=1.0;
-    private static double whole_lenght=2.0;
-    private static double dx = whole_lenght / (xpoints-1);
-
+    private static double whole_lenght=1.0;
+    private static double dx = whole_lenght / (xpoints - 1);
     // число точек на процесс
-    private static double local_n = xpoints/maxGlobalWorkSize;
+    private static int local_n = xpoints/maxGlobalWorkSize;
     
 
     private static cl_program program;
     private static cl_kernel kernel;
     private static cl_context context;
     private static cl_command_queue commandQueue;
-    private static cl_mem memObjects[] = new cl_mem[3];
+    private static cl_mem memObjects[] = new cl_mem[4];
 
-    
     /**
      * Main part of the Host Part.
      *
@@ -57,40 +53,27 @@ public class HostPart {
     public static void main(String args[]) {
         // Create input- and output data
         double[] U_plus = new double[xpoints];
-
-
         //массив произедения элементов
-
-
         double[] U_minus=new double[xpoints];
-        double[] D = new double[xpoints];
-        // создать два буффера и использовать их как начальные и конечные значения
-         
-         // round целочисленное округление
-         //int T=(int)Math.round(tmax/dt);
-         // пустить ступеньку как полуширина гауса  высота 1 
-          //double amplitude = 30000; //берём максимальную возможную амплитуду
-	  //double freq_Hz = 50; 
-          //double S_RATE=1;
+        double[] U_right_plus=new double[maxGlobalWorkSize];
+        double[] U_right_minus=new double[maxGlobalWorkSize];
+
           IntStream.range(0, xpoints).forEach((i) -> {
-          U_plus[i]=1/(0.1*Math.sqrt(2*Math.PI)*Math.exp((i*dx-0.5)*(i*dx-0.5)/(2*0.01))+(Math.random()*1e-12));
-          U_minus[i]=(Math.random()*1e-12);
-          D[i]=Math.random()*1e-5;
+          U_plus[i]=1/(0.1*Math.sqrt(2*Math.PI)*Math.exp((i*dx-0.5)*(i*dx-0.5)/(2*0.01))+(Math.random()*1e-10));
+          // U_plus[i]+=(int)(amplitude * Math.sin((float)(2*Math.PI*i*freq_Hz/S_RATE))+(Math.random()*1e-6));
+          U_minus[i]=(Math.random()*1e-10);
+
         });
-       
-        try (final FileWriter writer = new FileWriter("/Users/vladimirlozickiy/Desktop/Laser/src/main/java/kernels/temp.csv", false))
-        {
-            for (int i = 0; i < U_plus.length; i++)
-            {
-                final String s = Double.toString(U_plus[i]);
-                writer.write(s);
-                writer.write(System.lineSeparator());
-            }
-        }
-        catch(IOException e) {
-            System.out.println(e.getMessage());
-        }
-        
+        IntStream.range(0, maxGlobalWorkSize).forEach((i) -> {
+            U_right_plus[i]=0;
+        });
+        IntStream.range(0, maxGlobalWorkSize).forEach((i) -> {
+            U_right_minus[i]=0;
+        });
+
+        WritetoFile("/Users/vladimirlozickiy/Desktop/Laser/src/main/java/kernels/temporary.csv", U_plus);
+
+
         double initialWave=0;
         for (int p=0; p<xpoints;p++)
         { 
@@ -98,24 +81,18 @@ public class HostPart {
            
             
         }
-       // Pointer srcA = Pointer.to(temporaryRight);
-      //  Pointer srcB = Pointer.to(temporaryLeft);
-        // right правая волна
-        //tmp лева] волна
+
         Pointer u_plus = Pointer.to(U_plus);
         Pointer u_minus = Pointer.to(U_minus);
-        Pointer d = Pointer.to(D);
-        initialize(u_plus, u_minus, d);
-       // initialize(right, left);
-
-        // Set the arguments for the kernel
-        // добавить новый элемент(счетчик элементов массива)
+        Pointer u_right_plus = Pointer.to(U_right_plus);
+        Pointer u_right_minus = Pointer.to(U_right_minus);
         
-        
+        initialize(u_plus, u_minus, u_right_plus,u_right_minus);
         //Задаем аргументы ядра
         clSetKernelArg(kernel, 0, Sizeof.cl_mem, Pointer.to(memObjects[0]));
         clSetKernelArg(kernel, 1, Sizeof.cl_mem, Pointer.to(memObjects[1]));
         clSetKernelArg(kernel, 2, Sizeof.cl_mem, Pointer.to(memObjects[2]));
+        clSetKernelArg(kernel, 3, Sizeof.cl_mem, Pointer.to(memObjects[3]));
 
         // Set the work-item dimensions
         long global_work_size[] = new long[]{maxGlobalWorkSize};
@@ -126,77 +103,20 @@ public class HostPart {
         /*
           выполнение ядра
         */ 
-       
-   
 
    for(int t=0; t<tmax; t+=xpoints*dt )
    {
         clEnqueueNDRangeKernel(commandQueue, kernel, 1, null, global_work_size, local_work_size, 0, null, null);
         // Read the output data
         // чтение из буфер
-        
+
         clEnqueueReadBuffer(commandQueue, memObjects[0], CL_TRUE, 0, xpoints * Sizeof.cl_double, u_plus, 0, null, null);
         clEnqueueReadBuffer(commandQueue, memObjects[1], CL_TRUE, 0, xpoints * Sizeof.cl_double, u_minus, 0, null, null);
-        clEnqueueReadBuffer(commandQueue, memObjects[2], CL_TRUE, 0, xpoints * Sizeof.cl_double, d, 0, null, null);
-       // clEnqueueReadBuffer(commandQueue, memObjects[3], CL_TRUE, 0, xpoints * Sizeof.cl_double, left, 0, null, null);
+        clEnqueueReadBuffer(commandQueue, memObjects[2], CL_TRUE, 0, maxGlobalWorkSize * Sizeof.cl_double, u_right_plus, 0, null, null);
+        clEnqueueReadBuffer(commandQueue, memObjects[3], CL_TRUE, 0, maxGlobalWorkSize * Sizeof.cl_double, u_right_minus, 0, null, null);
            // запись в файл правой волны
-//         try (final FileWriter writer = new FileWriter("/Users/vladimir/Desktop/LaserDynamicsModelling/src/main/U_plus.csv", false))
-//        {
-//             for (int i =0; i < U_plus.length; i++)
-//            {
-//                final String s = Double.toString(U_plus[i]);
-//                writer.write(s);
-//                writer.write(System.lineSeparator());
-//                //U_plus[i]=U_plus[i]+(Math.random()*1e-12);
-//            }
-//          // System.out.println("u_plus.lenght" + u_plus.length);
-//        }
-//        catch(IOException e) {
-//            System.out.println(e.getMessage());
-//         }
-
-             try (final FileWriter writer = new FileWriter("/Users/vladimirlozickiy/Desktop/Laser/src/main/java/kernels/U_minus.csv", false))
-        {
-             for (int i=U_minus.length-1; i>=0; i--)
-            {
-                final String s = Double.toString(U_minus[i]);
-                writer.write(s);
-                writer.write(System.lineSeparator());
-                //U_plus[i]=U_plus[i]+(Math.random()*1e-12);
-            }
-          // System.out.println("u_plus.lenght" + u_plus.length);
-        }
-        catch(IOException e) {
-            System.out.println(e.getMessage());
-         }
-             
-             
-           try (final FileWriter writer = new FileWriter("/Users/vladimirlozickiy/Desktop/Laser/src/main/java/kernels/U_plus.csv", false))
-        {
-            for (int i = 0; i < U_plus.length; i++)
-            {
-                final String s = Double.toString(U_plus[i]);
-                writer.write(s);
-                writer.write(System.lineSeparator());
-               // U_minus[i]=U_minus[i]+(Math.random()*1e-12);
-            }
-        }
-        catch(IOException e) {
-            System.out.println(e.getMessage());
-        }
-
-       try (final FileWriter writer = new FileWriter("/Users/vladimirlozickiy/Desktop/Laser/src/main/java/kernels/D.csv", false))
-       {
-           for (int i = 0; i < D.length; i++)
-           {
-               final String s = Double.toString(D[i]);
-               writer.write(s);
-               writer.write(System.lineSeparator());
-           }
-       }
-       catch(IOException e) {
-           System.out.println(e.getMessage());
-       }
+       WritetoFile("/Users/vladimirlozickiy/Desktop/Laser/src/main/java/kernels/U_minus.csv", U_minus);
+       WritetoFile("/Users/vladimirlozickiy/Desktop/Laser/src/main/java/kernels/U_plus.csv", U_plus);
 
    }
        /*
@@ -213,15 +133,14 @@ public class HostPart {
          System.out.println("волна с шагом dt: " + oneStep);
          System.out.println("разность : " + difference);
 
-         
         //output the results to the console
         
-         //IntStream.range(0, U_plus.length).mapToDouble(i -> U_plus[i]).forEach(System.out::println);
+       // IntStream.range(0, U_plus.length).mapToDouble(i -> U_plus[i]).forEach(System.out::println);
         //IntStream.range(0, u_minus.length).mapToDouble(i -> u_minus[i]).forEach(System.out::println);
        
       
          shutdown();
-    } 
+    }
 
     /**
      * initializes all main parts of the program as kernel, program, command queue, context etc.
@@ -232,11 +151,11 @@ public class HostPart {
     
     //инициализация основных частей программы
     //////
-    private static void initialize(final Pointer pointerA, final Pointer pointerB, final Pointer pointerD) {
+    private static void initialize(final Pointer pointerA, final Pointer pointerB, Pointer pointerC, Pointer pointerD) {
         // The platform, device type and device number that will be used
         	
         final int platformIndex = 0;
-        final long deviceType = CL_DEVICE_TYPE_CPU;
+        final long deviceType = CL_DEVICE_TYPE_ALL;
         final int deviceIndex = 0;
 
         // Enable exceptions and subsequently omit error checks in this sample
@@ -279,36 +198,22 @@ public class HostPart {
         // создаем буфер
         memObjects[0] = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, Sizeof.cl_double * xpoints, pointerA, null);
         memObjects[1] = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, Sizeof.cl_double * xpoints, pointerB, null);
-        memObjects[2] = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, Sizeof.cl_double * xpoints, pointerD, null);
-        String programSource =
+        memObjects[2] = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, Sizeof.cl_double * xpoints, pointerC, null);
+        memObjects[3] = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, Sizeof.cl_double * xpoints, pointerD, null);
+
+      
+        String programSource;
+       programSource = 
                readFile("/Users/vladimirlozickiy/Desktop/Laser/src/main/java/kernels/LaserDynamics.cl")
-                       .replace( "{{xpoints}}", String.format(Locale.US,"%d",xpoints) )
-                       .replace("{{save_freq}}", String.format(Locale.US,"%d", save_freq) )
+                       .replace("{{xpoints}}", String.format(Locale.US,"%d",xpoints) )
+                       .replace("{{save_freq}}", String.format(Locale.US,"%f", save_freq) )
                        .replace("{{maxGlobalWorkSize}}", String.format(Locale.US,"%d", maxGlobalWorkSize) )
                        .replace("{{R_left}}", String.format(Locale.US,"%f",Rleft) )
                        .replace("{{R_right}}", String.format(Locale.US,"%f",Rright) )
                        .replace("{{dt}}", String.format(Locale.US,"%f",dt) )
                        .replace("{{dx}}", String.format(Locale.US,"%f",dx) )
-                       .replace("{{local_n}}", String.format(Locale.US,"%f",local_n) )
+                       .replace("{{local_n}}", String.format(Locale.US,"%d",local_n) )
                ;
-
-           // equals 
-           // метод equals сравнение строк
-       // String programSource;
-        /*if (Constants.kernelCreator.equals(Constants.KernelCreator.CODE.name())) {
-            
-           // Create kernel file from the source code
-           // создаем файл ядра из исходного кода
-            
-           Resonator resonator = new ResonatorBuilder().buildResonator();
-            programSource = new KernelBuilder(resonator).buildKernel();
-        } else {
-           // Create the program from the existing kernel file
-            //  Создает программу из существующего файла ядра
-            int lalala=400;
-            programSource = readFile(Constants.clFileName).replace("{{lalala}}", String.format("%d",lalala) ); 
-        }*/
-
 
 
 
@@ -367,6 +272,25 @@ public class HostPart {
     /**
      * Release created kernel, program, command queue and context.
      */
+    /**
+     *
+     * @param filename
+     * @param s
+     */
+    private static void  WritetoFile(String filename, double[] s ){
+        try (final FileWriter writer = new FileWriter(filename, false))
+        {
+            for (int i = 0; i <s.length; i++)
+            {
+                final String l = Double.toString(s[i]);
+                writer.write(l);
+                writer.write(System.lineSeparator());
+            }
+        }
+        catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     private static void shutdown() {
         // Release kernel, program, and memory objects
         releaseObjects(memObjects);
--- src/main/java/kernels/LaserDynamics.cl
+++ src/main/java/kernels/LaserDynamics.cl
@@ -14,7 +14,7 @@ static double x_max(double x, double R_max)
 }
 
 
-__kernel void laserDynamics(__global double * U_plus,__global double * U_minus,__global double * D)
+__kernel void laserDynamics(__global double * U_plus,__global double * U_minus, __global double * U_right_plus,  __global double * U_right_minus)
 {
 	  int pid = get_global_id(0);
       double R_max = {{R_right}};
@@ -32,28 +32,25 @@ __kernel void laserDynamics(__global double * U_plus,__global double * U_minus,_
          int finish = local_N + start;
          // вычисления будут проводиться в приватной (быстрой) памяти
           	__private double u_plus[local_N + 1]; // на всех +1 для приватных точек; для унификации
-          	__private double d[local_N + 1];
-          	__private double u_minus[local_N + 1];
-          	__private double u_tmp_plus[local_N + 1];
+          	__private double u_minus[local_N  + 1];
+          	__private double u_tmp_plus[local_N  + 1];
           	__private double u_tmp_minus[local_N + 1];
-
-          	__private double u_half_plus[local_N + 1];
-          	__private double u_half_minus[local_N + 1];
+          	__private double u_half_plus[local_N  + 1];
+          	__private double u_half_minus[local_N  + 1];
 
           	// общая память
           	// начальный слой
           	__local double u_left_plus[size]; // не size - 1, т. к. нужны ещё точки для граничных условий
-          	__local double u_right_plus[size];
           	__local double u_left_minus[size];
-          	__local double u_right_minus[size];
+
+
 
           	// worker'ы разбирают массивы
           	for (int i = start; i < finish; i++)
           	{
           		u_plus[i] = U_plus[pid * local_N + i - start];
           		u_minus[i] = U_minus[pid * local_N + i - start];
-
-          	}
+            }
 
           	// вычисления
 
@@ -65,31 +62,33 @@ __kernel void laserDynamics(__global double * U_plus,__global double * U_minus,_
          	 	u_left_plus[pid] = u_plus[start];
          	 	u_left_minus[pid] = u_minus[start];
 
-         		u_right_plus[pid] = u_plus[local_N - 1 + start];
-         	 	u_right_minus[pid] = u_minus[local_N - 1 + start];
+
+         		U_right_plus[pid] = u_plus[local_N - 1 + start];
+         	 	U_right_minus[pid] = u_minus[local_N - 1 + start];
 
          		// виртуальные точки и граничные условия
          	 	if (pid == 0)
          	 	{
-         	 		u_plus[0] = x_0(u_right_minus[size - 1], R_0);
-         	 		u_minus[0] = x_max(u_right_plus[size - 1], R_max);
+         	 		u_plus[0] = x_0(U_right_minus[size - 1], R_0);
+         	 		u_minus[0] = x_max(U_right_plus[size - 1], R_max);
          	 	}
          	 	if (pid == (size - 1))
          	 	{
          	 		u_plus[local_N] = x_max(u_left_minus[0], R_max);
          	 		u_minus[local_N] = x_0(u_left_plus[0], R_0);
          	 	}
+         	 	barrier(CLK_LOCAL_MEM_FENCE);
+         	 	barrier(CLK_LOCAL_MEM_FENCE);
 
-         	 	barrier(CLK_LOCAL_MEM_FENCE | CLK_GLOBAL_MEM_FENCE);
 
          	 	// половинный слой
          		for(int i = 0; i < local_N + 1; i++)
          		{
          	 		if(pid != 0 && i == 0)
          	 		{
-         	 			u_n_plus = u_right_plus[pid - 1];
+         	 			u_n_plus = U_right_plus[pid-1];
          				u_np1_plus = u_plus[i];
-         	 			u_n_minus = u_right_minus[pid - 1];
+         	 			u_n_minus = U_right_minus[pid - 1];
          				u_np1_minus = u_minus[i];
          	 		}
          	 		if(pid != size - 1 && i == local_N)
@@ -113,16 +112,15 @@ __kernel void laserDynamics(__global double * U_plus,__global double * U_minus,_
          			// разности для полного слоя
          			u_tmp_plus[i] = u_np1_plus - u_n_plus;
          			u_tmp_minus[i] = u_np1_minus - u_n_minus;
-         		}
-
+}
          		// полный слой
          		for(int i = start; i < finish; i++)
          		{
          			if(pid != 0 && i == 0)
          			{
-         				u_n_plus = u_right_plus[pid - 1];
+         				u_n_plus = U_right_plus[pid - 1];
          				u_np1_plus = u_plus[i + 1];
-         				u_n_minus = u_right_minus[pid - 1];
+         				u_n_minus = U_right_minus[pid - 1];
          				u_np1_minus = u_minus[i + 1];
          			}
          			if(pid != size - 1 && i == local_N - 1 + start)
@@ -140,22 +138,22 @@ __kernel void laserDynamics(__global double * U_plus,__global double * U_minus,_
          				u_np1_minus = u_minus[i + 1];
          			}
 
+
          			u_plus[i] -= cl_dt / cl_dx * u_tmp_plus[i - start]
          							- ((u_tmp_plus[i - start] / (u_np1_plus - u_plus[i] * 0.999999999)
          							* u_half_plus[i + 1 - start]) - u_half_plus[i - start]);
 
-
          			u_minus[i] -= cl_dt / cl_dx * u_tmp_minus[i - start]
          							- ((u_tmp_minus[i - start] / (u_np1_minus - u_minus[i] * 0.999999999)
          							* u_half_minus[i + 1 - start]) - u_half_minus[i - start]);
+
+
          		}
          	}
-
          	for (int i = start; i < finish; i++)
          	{
          		U_plus[pid * local_N + i - start] = u_plus[i];
          		U_minus[pid * local_N + i - start] = u_minus[i];
-         		D[i];
          	}
 
 
